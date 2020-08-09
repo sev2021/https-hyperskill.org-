c471405b2c98578cf75f95f934b49415d1a16c8f
code <33802433+codeacade@users.noreply.github.com>
2020-08-09 01:24:00 +0100

    second update

--- 2020_07_Bank/first.py
+++ 2020_07_Bank/first.py
@@ -1,8 +1,16 @@
 # Write your code here
 import random
 all_clients = {"0":"0000"}
-print("1. Create an account\n2. Log into account\n0. Exit")
-client_choice = input()
+
+while True:
+    print("1. Create an account\n2. Log into account\n0. Exit")
+    client_choice = input()
+    if client_choice == "0":
+        exit()
+    if client_choice == "1":
+        all_clients = add_client(all_clients)
+    if client_choice == "2":
+        log_client(all_clients)
 
 def add_client(all_clients = all_clients):
     new_account = "0"
@@ -17,6 +25,9 @@ def add_client(all_clients = all_clients):
     print("Your card PIN:")
     print(all_clients[new_account])
     return all_clients
+
+def log_client(all_clients = all_clients):
+    
     
 
     
