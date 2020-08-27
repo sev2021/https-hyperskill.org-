7da74de72d8156e77361444737b18d46ffcf65e5
code <33802433+codeacade@users.noreply.github.com>
2020-08-27 10:22:54 +0100

    access to random.txt file added

--- rock-paper-scissors/ref.py
+++ rock-paper-scissors/ref.py
@@ -3,29 +3,39 @@ def g():
 
 	moves = ["rock", "paper", "scissors", "!exit", "!rating"]
 	winner = ["rockscissors", "paperrock", "scissorspaper"]
-	playerp = 0
-	
+
+	player_name = input("Enter your name: ")
+	print(f"Hello, {player_name}")
+
+	players = open("rating.txt").readlines()
+
+	if player_name in str(players):
+		player_rating = int([i.split() for i in players if player_name in i][0][1])
+	else:
+		player_rating = 0
+
 	while True:
 		player = input()
-		
-		if player not in moves:
+
+		while player not in moves:
 			print("Invalid input")
-			continue
+			player = input()
+
 		if player == "!exit":
 			break
 		if player == "!rating":
-			print(f"Your rating: {playerp}")
+			print(f"Your rating: {player_rating}")
 			continue
-		
+
 		computer = random.choice(moves[:3])
-		print(player, "-", computer, "-", playerp)
-		
+
 		if player == computer:
 			print(f"There is a draw ({computer})")
-			playerp += 50
+			player_rating += 50
 		elif (player + computer) in winner:
 			print(f"Well done. Computer chose {computer} and failed")	
-			playerp += 100
+			player_rating += 100
 		else:
 			print(f"Sorry, but computer chose {computer}")
+
 	print("Bye!")
