d575041eaf608ec30703ea2f09b981558b38f839
code <33802433+codeacade@users.noreply.github.com>
2020-12-16 02:09:16 +0000

    Horrorsdays over...

---
+++ 2020_11_Alchemy/dict_list.py
@@ -0,0 +1,12 @@
+# the list "meals" is already defined
+# meals = [
+#         {"title": "Oatmeal pancakes with apple and cinnamon", "kcal": 370},
+#         {"title": "Italian salad with fusilli and ham", "kcal": 320},
+#         {"title": "Bulgur with vegetables", "kcal": 350},
+#         {"title": "Curd souffle with lingonberries and ginger", "kcal": 225},
+#         {"title": "Oatmeal with honey and peanuts", "kcal": 440}]
+#  credit to Haritha Gurram
+
+# the list "meals" is already defined
+# your code here
+print(sum(meal['kcal'] for meal in meals))
