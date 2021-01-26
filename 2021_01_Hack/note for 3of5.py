2f493e9afe62924025233bb3686bb63c7932d50d
code <33802433+codeacade@users.noreply.github.com>
2021-01-26 13:26:29 +0000

    better fix with "product" and "zip"

--- 2021_01_Hack/note for 3of5.py
+++ 2021_01_Hack/note for 3of5.py
@@ -5,8 +5,16 @@
 import itertools
 
 is_pass = "abcde"
-print("Test word:",is_pass)
 
-
+#################  !!!!!!!!!  THIS IS TOO MUCH MEMORY CONSUMING !!!!!!!!!!!!!! ########################
 for k in list(itertools.product([0,1],repeat=len(is_pass))):  #  iterate possible 1-0
     print(*[(ch,ch.upper())[k[i]] for i,ch in enumerate(is_pass)])  #  translate 1-0 to low-up case
+########################################################################################################
+
+############  !! USE THIS INSTEAD !!  ################
+print(*ii.product(*zip(is_pass, is_pas.upper())))
+
+## zip("abc", "ABC") -> ("a","A"),("b","B"),("c","C")
+## product(argument1, argument2, .... argument-n)
+## zip with "*" prefic will unload into product with all its arguments
+######################################################
