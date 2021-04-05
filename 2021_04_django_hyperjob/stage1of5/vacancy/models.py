41e5fc82530c453b479304c4c9db98577e6edd01
code <33802433+codeacade@users.noreply.github.com>
2021-04-05 02:41:25 +0100

    this was task of stage1of5

--- 2021_04_django_hyperjob/stage1of5/vacancy/models.py
+++ 2021_04_django_hyperjob/stage1of5/vacancy/models.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 # Create your models here.
 class Vacancy(models.Model):
-  description = models.CharField(max_length=1024)
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  description = models.CharField(max_length=1024)                                 ##  self explanatory
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)    ##  no idea where settings.AUTH_USER_MODEL came from
