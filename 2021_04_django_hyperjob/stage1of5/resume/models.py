71f0841345701b032d6433c16de79e39e9b8ad3e
code <33802433+codeacade@users.noreply.github.com>
2021-04-05 02:40:32 +0100

    this was task for stage1of5

--- 2021_04_django_hyperjob/stage1of5/resume/models.py
+++ 2021_04_django_hyperjob/stage1of5/resume/models.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 # Create your models here.
 class Resume(models.Model):
-  description = models.CharField(max_length=1024)
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  description = models.CharField(max_length=1024)     ##  self explanatory
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)   ## no idea where settings.AUTH_USER_MODEL came from
 
